001f65a35b1587894a89e7768c81e995f934e2fd
Hoàng Công Nhật Nam <1512061@hcmut.edu.vn>
2019-04-02 21:45:36 +0700

    token,get by history,id

--- server.py
+++ server.py
@@ -5,6 +5,15 @@ import datetime
 import requests
 import json
 
+
+from datetime import date, datetime
+
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+    raise TypeError ("Type %s not serializable" % type(obj))
 # Connect to the database
 connection = pymysql.connect(host='sql12.freemysqlhosting.net',
                              user='sql12283501',
@@ -23,35 +32,58 @@ def selectDB():
         return result
 
 
-def inserDB(value, bbox):
+def inserDB(value, bbox,token):
     with connection.cursor() as cursor:
         # Create a new record
-        sql = "INSERT INTO images (value, timedetail,meta1) VALUES (%s, %s,%s)"
-        cursor.execute(sql, (value, datetime.datetime.now(), bbox+""))
+        sql = "INSERT INTO images (value, timedetail,meta1,token) VALUES (%s, %s,%s,%s)"
+        cursor.execute(sql, (value, datetime.datetime.now(), bbox+"",token))
 
     # connection is not autocommit by default. So you must commit to save
     # your changes.
     connection.commit()
+def selectDBbyToken(token):
+    with connection.cursor() as cursor:
+        # Read a single record
+        sql = "SELECT * FROM images WHERE token=%s"
+        cursor.execute(sql,(token))
+        result = cursor.fetchall()
+        return result
 
+def selectDBbyId(id):
+    with connection.cursor() as cursor:
+        # Read a single record
+        sql = "SELECT * FROM images WHERE id=%s"
+        cursor.execute(sql,(id))
+        result = cursor.fetchall()
+        return result
 
 url = 'https://bk15api.herokuapp.com/api'
 IMAGEKEY = 'image'
+TOKENKEY = 'token'
 BBOXKEY = "bbox"
-
+GETTYPEKEY="gettype"
+DATAGETKEY="data"
 
 class ControllerApi(Resource):
 
     def get(self):
-        response = requests.get(url)
-        json_data = json.loads(response.text)
-        inserDB(json_data[IMAGEKEY], json_data[BBOXKEY])
-        return {IMAGEKEY: json_data[IMAGEKEY], BBOXKEY: json_data[BBOXKEY]}
+        result=""
+        gettype = request.args.get(GETTYPEKEY)
+        if gettype=="history":
+            token = request.args.get(DATAGETKEY)
+            result= selectDBbyToken(token)
+        elif gettype=="idget":
+            id = request.args.get(DATAGETKEY)
+            result = selectDBbyId(id)
+        return json.dumps(result, default=json_serial)
+
 
     def post(self):
+        token = json.loads(request.data.decode('utf8'))[TOKENKEY]
         imstring = json.loads(request.data.decode('utf8').replace("'", '"'))[IMAGEKEY]
         response = requests.post(url, json={"image": imstring})
         json_data = json.loads(response.text)
-        inserDB(json_data[IMAGEKEY], json_data[BBOXKEY])
+        inserDB(json_data[IMAGEKEY], json_data[BBOXKEY],token)
         return {IMAGEKEY: json_data[IMAGEKEY], BBOXKEY: json_data[BBOXKEY]}
 
     def put(self):
