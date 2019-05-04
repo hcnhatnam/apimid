2dc3e3c9e65cc72d52ef7b0084293b5428a92fc9
Hoàng Công Nhật Nam <1512061@hcmut.edu.vn>
2019-05-04 11:45:48 +0700

    a

--- server.py
+++ server.py
@@ -133,8 +133,8 @@ class ControllerApi(Resource):
 
         print("urlImage",urlImage)
 
-        response = requests.post(url, json={"urlimage": urlImage},verify=False)
-        # response=requests.get(url,verify=False)
+        # response = requests.post(url, json={"urlimage": urlImage},verify=False)
+        response=requests.get(url,verify=False)
         print("response",response)
         json_data = json.loads(response.text)
         id=inserDB(id,urlImage, json_data[BBOXKEY],token)
